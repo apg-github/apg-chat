69bb432565df9d306910a8a335e9d6937c018e96
APG <apg.prywatny@gmail.com>
2021-04-14 21:30:00 +0200

    added initial components and rooms

--- src/App.js
+++ src/App.js
@@ -5,15 +5,20 @@ import "firebase/firestore";
 import "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollectionData } from "react-firebase-hooks/firestore";
+import { isCompositeComponent } from "react-dom/test-utils";
 
-firebase.initializeApp({
-  apiKey: "AIzaSyApsdXWCslizIMjbe4A3RSj6eebLC7TCP8",
-  authDomain: "apg-chat.firebaseapp.com",
-  projectId: "apg-chat",
-  storageBucket: "apg-chat.appspot.com",
-  messagingSenderId: "911590173882",
-  appId: "1:911590173882:web:38f73246b4d6f148e36683",
-});
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: "AIzaSyApsdXWCslizIMjbe4A3RSj6eebLC7TCP8",
+    authDomain: "apg-chat.firebaseapp.com",
+    projectId: "apg-chat",
+    storageBucket: "apg-chat.appspot.com",
+    messagingSenderId: "911590173882",
+    appId: "1:911590173882:web:38f73246b4d6f148e36683",
+  });
+} else {
+  firebase.app(); // if already initialized, use that one
+}
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
@@ -26,18 +31,21 @@ function SignIn() {
   return <button onClick={signInWithGoogle}>Sign in with Google</button>;
 }
 
-function signOut() {
+function SignOut() {
   return auth.currentUser && <button onClick={() => auth.signOut()}>Sign out from chat</button>;
 }
 
-function ChatRoom() {
+function ChatRoom(props) {
   const dummy = useRef();
-  const messageRef = firestore.collection("messages");
+  const messageRef =
+    props.room === 1
+      ? firestore.collection("channel_alpha")
+      : firestore.collection("channel_beta");
   const query = messageRef.orderBy("createdAt").limit(25);
 
   const [messages] = useCollectionData(query, { idField: "id" });
   const [formValue, setFormValue] = useState("");
-  console.log(messages);
+
   const sendMessage = async (e) => {
     e.preventDefault();
 
@@ -56,7 +64,7 @@ function ChatRoom() {
 
   return (
     <>
-      <main>
+      <main style={{ paddingTop: "2em" }}>
         {messages && messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
         <span ref={dummy}></span>
       </main>
@@ -77,7 +85,6 @@ function ChatRoom() {
 
 function ChatMessage(props) {
   const { text, uid, photoURL } = props.message;
-  console.log(text);
   const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
 
   return (
@@ -97,12 +104,25 @@ function ChatMessage(props) {
 
 function App() {
   const [user] = useAuthState(auth);
-
+  const [room, changeRoom] = useState(1)
+  
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header style={{ padding: "2.5em" }}>
+        <h1>⚛️🔥💬</h1>
+        {user ? (
+          <>
+            <button onClick={() => changeRoom(1)}>Room 1</button>
+            <button onClick={() => changeRoom(2)}>Room 2</button>
+          </>
+        ) : (
+          <h1>APG Chat - sign in first</h1>
+        )}
 
-      <section>{user ? <ChatRoom /> : <SignIn />}</section>
+        <SignOut />
+      </header>
+
+      <section>{user ? <ChatRoom room={room} /> : <SignIn />}</section>
     </div>
   );
 }
